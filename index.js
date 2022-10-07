af8cf4bcf16d92644261398cb9cd2b141f21d987
DESKTOP-HBOHOKA\Isita <rfabi442@gmail.com>
2022-10-06 20:01:49 -0500

    Backend listo

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+
+//DB CONFIG
+const {dbConnection } = require('./database/config');
+dbConnection(); 
+
+
+//APP DE EXPRESS
+const app = express();
+
+//LECTURA Y PARSEO DEL BODY (NOTA: Middleware, funcion que se ejecuta cuando pasa por aqui el codigo)
+app.use(express.json());
+
+
+//NODE SERVER
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/sockets')
+
+
+//Path publico
+const publicPath = path.resolve(__dirname,'public');
+app.use(express.static(publicPath));
+
+//MIS RUTAS
+app.use('/api/login',require('./routes/auth'));
+
+
+
+server.listen(process.env.PORT,( err )  => {
+
+    if(err) throw new Error(err);
+    console.log('Servidor corriendo en puerto!!!,',process.env.PORT);
+
+});
